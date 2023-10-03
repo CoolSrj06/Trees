b5bdcb4203286fc1ad7c48fa3476cc99e0b20a09
CoolSrj06 <srijanmaurya6602@gmail.com>
2023-10-03 22:07:53 +0000

    Working on ALV Codes

--- AVL.java
+++ AVL.java
@@ -1,3 +1,33 @@
 class ALV{
-  
+  class Node {
+    int val;
+    Node left;
+    Node right;
+
+    public Node(int val){
+      this.val=val;
+    }
+
+    private Node root;
+    public void insert(int val){
+      root=insert(root,val);
+    }
+    private Node insert(Node node,int val){
+      if(node == null){
+        return node;
+      }
+
+      if(node.val>val){
+        node.left=new Node(val);
+      }
+
+      if(node.val<val){
+        node.left=new Node(val);
+      }
+
+      return rotate(node);
+    }
+
+    
+  }
 }
--- Main.java
+++ Main.java
@@ -3,11 +3,11 @@ import java.util.*;
 class Main{
   public static void main (String args[]){
     Scanner sc=new Scanner(System.in);
-    BinaryTree bt =new BinaryTree();
+    //BinaryTree bt =new BinaryTree();
 
     //bt.createRoot(sc);
     //bt.display();
-
+    /**
     BST bst=new BST();
     int arr1[]={1,2,3,4,5};
     int arr[]={ 5, 2, 7, 1, 4, 6, 9, 8, 3, 10 };
@@ -15,5 +15,8 @@ class Main{
     
     bst.populatedSorted(arr1);
     bst.display();
+    **/
+
+    ALV avl=new AVL();
   }
 } 
